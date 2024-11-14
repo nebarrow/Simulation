bdd108116ea2317cd0aa13f6e586c73eb7de3cd0
nebarrow <101344206+nebarrow@users.noreply.github.com>
2024-11-15 01:34:27 +0300

    refactor: removed the creation of a variable bfs and passing it to the method move

--- actions/move/MoveAction.java
+++ actions/move/MoveAction.java
@@ -17,14 +17,13 @@ public class MoveAction extends Action {
     @Override
     public void execute() {
         Set<Coordinate> creatureCoordinate = map.getCoordinatesByEntity(Creature.class);
-        BreadthFirstSearch searcher = new BreadthFirstSearch(map);
         for (Coordinate coordinate : creatureCoordinate) {
             if (!map.isCellEmpty(coordinate)) {
                 Entity entity = map.getEntityByCoordinates(coordinate);
                 Creature creature = (Creature) entity;
                 int countOfMoves = creature.getSpeed();
                 for (int numberOfMove = 0; numberOfMove < countOfMoves; numberOfMove++) {
-                    creature.move(map, coordinate, searcher);
+                    creature.move(map, coordinate);
                 }
             }
         }
