6274131b374f46cd5ce1800db014edb4971004df
nebarrow <101344206+nebarrow@users.noreply.github.com>
2024-11-15 01:15:04 +0300

    refactor: delete Earth class

--- sprite/SpriteChoicer.java
+++ sprite/SpriteChoicer.java
@@ -4,7 +4,6 @@ import animals.models.Entity;
 import animals.entities.Herbivore;
 import animals.entities.Predator;
 import objects.dynamics.NutritiousPlant;
-import objects.statics.Earth;
 import objects.statics.Rock;
 import objects.statics.Tree;
 
@@ -15,7 +14,6 @@ public class SpriteChoicer {
     private final Map<Class<? extends Entity>, EntitySprite> entitySprites = new HashMap<>();
 
     public SpriteChoicer() {
-        entitySprites.put(Earth.class, EntitySprite.EARTH);
         entitySprites.put(Predator.class, EntitySprite.PREDATOR);
         entitySprites.put(Herbivore.class, EntitySprite.HERBIVORE);
         entitySprites.put(NutritiousPlant.class, EntitySprite.PLANT);
@@ -24,7 +22,10 @@ public class SpriteChoicer {
     }
 
     public String getSpriteForEntity(Entity entity) {
+        if (entity == null) {
+            return EntitySprite.EARTH.getSymbol();
+        }
         EntitySprite sprite = entitySprites.get(entity.getClass());
-        return sprite.getSymbol();
+        return sprite != null ? sprite.getSymbol() : EntitySprite.EARTH.getSymbol();
     }
 }
